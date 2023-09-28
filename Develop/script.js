13dad224503a2da22b9ba7f07adc0ab177b61215
Evie Hoang <evie.h0325@gmail.com>
2023-09-28 10:24:15 -0700

    Revert back to previous commit and starting over.

--- Develop/script.js
+++ Develop/script.js
@@ -1,68 +1,27 @@
-// Get character length value from slider
-var slider = document.getElementById("myRange");
+//Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Show Value of slider
-var sliderValue = document.getElementById("myLength");
+// Various Arrays and variables
+var number = ["01234567890"];
+var specialChar = ["!@#$%^&*()-=_+<>?,./}{"];
+var alphaLower = ["abcdefghijklmnopqrstuvwxyz"];
+var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var inputLength ="";
+var symbols;
+var nums;
+var upperCase;
+var lowerCase;
 
-// Display the default slider value
-sliderValue.innerHTML = slider.value;
-slider.oninput = function () {
-  sliderValue.innerHTML = this.value;
-}
+//Generate Password
+function generatePassword(){
+  var inputLength = prompt("How long is your password? Choose a number between 8 and 128.");
 
-// // Variables for different characters.
-var upperCase = document.getElementById("upper");
-var lowCase = document.getElementById("lower");
-var numbs = document.getElementById("nums");
-var symbols = document.getElementById("symbols");
-
-var passwordText = document.getElementById("password");
-
-// Variables in relation to AASCII Code
-const upperChar = arrayRange(65, 90)
-const lowerChar = arrayRange(97, 122)
-const numChar = arrayRange(48, 57)
-const symbolChar = arrayRange(33, 47).concat(
-  arrayRange(58, 64)
-).concat(
-  arrayRange(91, 96)
-).concat(
-  arrayRange(123, 126)
-)
-// function goes through and grabs from refrenced ranges
-function arrayRange(low, high) {
-  const array = []
-  for (let i = low; i <= high; i++) {
-    array.push(i)
+  // if number is not between 8 and 128
+  while (inputLength < 8 ||inputLength >128){
+    alert ("Password length must be between 8 and 128. Please try again.");
+    var inputLength = prompt("How long is your password? Choose a number between 8 and 128.");
   }
-  return array
-}
 
-// stops form from submitting and refreshing page.
-passwordText.addEventListener('submit', e => {
-  e.preventDefault()
-  var slider = myRange.value;
-  var upperCase = upperCaseElement.checked;
-  var lowCase = lowCaseElement.checked;
-  var numbs = numbsElement.checked;
-  var symbols = symbolsElement.checked;
-  var password = generatePassword(upperCase, lowCase, numbs, symbols);
-
-  var passwordText = document.getElementById("password");
-  passwordText.value = password;
-})
-
-// Generate Password
-function generatePassword(upperCase, lowCase, numbs, symbols) {
-  var chara = lowerChar
-  if (upperCase) chara = chara.concat(upperChar)
-  if (numbs) chara = chara.concat(numChar)
-  if (symbols) chara = chara.concat(symbolChar)
-
-  var pass = ""
-  for (var i = 0; i < slider; i++) {
-    var characters = chara[Math.floor(Math.random() * chara.length)]
-    pass.push(String.fromchara(characters))
-  }
-  return pass.join("")
-}
+  // if number is valid proceed to choosing the parameters for the password.
+  
+} 
